57d0e8c1b9ae1ae7e79d037d599febacf9114c4f
sun <limyoursun@gmail.com>
2023-03-09 00:45:44 +0900

    list_delete

---
+++ js/todo.js
@@ -0,0 +1,28 @@
+const todoForm = document.getElementById("todo-form");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.getElementById("todo-list");
+
+function deleteToDo(event){
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(newTodo){
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  todoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+  paintToDo(newTodo);
+}
+todoForm.addEventListener("submit", handleToDoSubmit);
